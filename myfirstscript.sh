5eba2dcb4d8ad8420b6cb6f55ab40288bd80e9de
Jenkins Automation Server <jenkins@ip-172-31-45-38.us-east-2.compute.internal>
2019-03-16 22:24:24 +0000

    updated run script

--- myfirstscript.sh
+++ myfirstscript.sh
@@ -1,1 +1,1 @@
-ssh frontend-user@172.31.40.95 -v -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null 'touch /tmp/helloworld2'
+ssh frontend-user@172.31.40.95 -v -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "touch /tmp/$1"
